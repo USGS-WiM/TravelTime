7ecfec942ddda85075342241214615dff85fd293
kjacobsen16 <kjacobsen@contractor.usgs.gov>
2019-04-10 11:26:35 -0500

    modal forms fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { GetTimeoftravelService } from './services/get-timeoftravel.service';
+import { reach } from './reach';
 
 export interface DialogData {
   jobson: {
@@ -19,10 +21,9 @@ export interface DialogData {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'Time of Travel';
-  
+
   location: [];
   volume: number;
   distance: number;
@@ -31,9 +32,9 @@ export class AppComponent {
   discharge: number;
   MAflow: number;
   slope: number;
+  mylist = [];
 
-
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog) {}
 
   openDialog(): void {
     const dialogRef = this.dialog.open(AppComponentModal, {
@@ -58,11 +59,52 @@ export class AppComponent {
   templateUrl: './modal/modal.component.html',
   styleUrls: ['./modal/modal.component.css']
 })
-export class AppComponentModal {
+export class AppComponentModal implements OnInit {
+  mylist = [];
+  reach_reference: reach;
+  ini_mass: number;
+  ini_time: number;
+  new_reach: reach;
+  output: {};
 
   constructor(
     public dialogRef: MatDialogRef<AppComponentModal>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private _GetTimeoftravelService: GetTimeoftravelService
+  ) {}
+  ngOnInit(): any {
+    //on init, get the services for first reach, and add them as parameters to accordion
+    this._GetTimeoftravelService
+      .getReach() // get reach
+      .subscribe(data => (this.reach_reference = data)); //get service {description: Initial description}
+    this.new_reach = this.reach_reference;
+  }
+
+  onClick_addReach() {
+    //add class jobson to an array of items that has been iterated over on ui side
+    let newreach = new reach(this.reach_reference); //new Jobson reaches object that will store initial object
+    this.mylist.push(newreach); //push the object to the array of reaches
+  }
+
+  onClick_removeReachLast() {
+    //remove last reach
+    this.mylist.pop();
+  }
+
+  onClick_removeReach(index) {
+    //remove reach by id
+    if (index >= 0) {
+      this.mylist.splice(index, 1);
+    }
+  }
+
+  customTrackBy(index: number, obj: any): any {
+    return index;
+  }
+
+  onClick_postReach() {
+    this._GetTimeoftravelService.postReach(this.mylist, this.ini_mass, this.ini_time).subscribe(data => (this.output = data));
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
