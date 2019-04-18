32e0e6b464aa35e0e41b5543997d8895b2ee1d01
tsabitov <tsabitov@IGSACNEWLT0097>
2019-04-18 16:57:36 -0400

    Added deep copy constructor for a post call. Implemented check for required parameters, if one is missing it is dropped. After implementing deep copy, dropped parameters are still editable for a next call inside of accordion unlike without deep copy version.

--- src/app/services/get-timeoftravel.service.ts
+++ src/app/services/get-timeoftravel.service.ts
@@ -21,10 +21,48 @@ export class GetTimeoftravelService {
     this._myurl = "https://test.streamstats.usgs.gov/timeoftravelservices/jobsons?initialmassconcentration="+ini_mass+"&starttime="+ini_time;
     //https://test.streamstats.usgs.gov/timeoftravelservices/jobsons?initialmassconcentration=6000&starttime=5
     var reaches ={ };
-    mylist.map ((myObj, index) => {
-      reaches[index] = myObj
-    }) //list into proper format for a call
     
+    //deepCopyConstructor
+    function deepCopy(obj) {
+      var copy;
+  
+      // Handle the 3 simple types, and null or undefined
+      if (null == obj || "object" != typeof obj) return obj;
+      
+      // Handle Array
+      if (obj instanceof Array) {
+          copy = [];
+          for (var i = 0, len = obj.length; i < len; i++) {
+              copy[i] = deepCopy(obj[i]);
+          }
+          return copy;
+      }
+  
+      // Handle Object
+      if (obj instanceof Object) {
+          copy = {};
+          for (var attr in obj) {
+              if (obj.hasOwnProperty(attr)) copy[attr] = deepCopy(obj[attr]);
+          }
+          return copy;
+      }
+  
+      throw new Error("Unable to copy obj! Its type isn't supported.");
+    }
+
+    //deep copy of the list before call, and check for inputs
+    for (var i= 0; i<mylist.length; i++){
+      var myreach = <reach>deepCopy(mylist[i]);
+      reaches[i] = myreach;
+      reaches[i]['parameters'].map((parObj, iter)=>{
+        if (parObj['required'] == false && parObj['value'] == undefined)
+        {
+          reaches[i]['parameters'].splice(iter, 1);
+        }
+      })
+    }
+    
+    //post a call
     this.http.post<reach> (this._myurl, {reaches}) //something with services, slope returning as undefined
     .subscribe (data => console.log (data));
     
