7e5de156982ad22f1f0a00931aaa0e5d74b9cd3a
Gross <tgross@usgs.gov>
2019-04-25 16:06:57 -0600

    added results, clear button, additional items to accordion list, new styles

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -18,6 +18,8 @@ export class ModalComponent implements OnInit {
   ini_time: number;
   id = []; //array for reach id's
   output = [];
+  showResult: boolean;
+  arrayOut = [];
 
   constructor(
     public dialogRef: MatDialogRef<ModalComponent>,
@@ -28,17 +30,21 @@ export class ModalComponent implements OnInit {
   ngOnInit(): any {   //on init, get the services for first reach, and add them as parameters to accordion
     this._GetTimeoftravelService.getReach() // get reach
       .subscribe(data => this.reach_reference = data); //get service {description: Initial description}
+
+    this.showResult = false;
   }
 
   onClick_addReach() {   //add class jobson to an array of items that has been iterated over on ui side
     let newreach = new reach(this.reach_reference); //new Jobson reaches object that will store initial object
+    newreach.name = "Reach " + (this.id.length + 1);
     this.mylist.push(newreach);    //push the object to the array of reaches
     if (this.mylist.length > 1) { 
-      this.id.push(this.id[this.id.length-1] + 1); //take the last value of id and add one
+      this.id.push(this.id[this.id.length - 1] + 1); //take the last value of id and add one
     } else {
       this.id = [];
       this.id.push(1); //at any time when only one reach start id from 1 
     }
+    console.log(this.mylist);
   };
 
   onClick_removeReachLast() {  //remove last reach
@@ -59,14 +65,33 @@ export class ModalComponent implements OnInit {
   onClick_postReach() {
     this._GetTimeoftravelService.postReach(this.mylist, this.ini_mass, this.ini_time)
       .subscribe(data => this.output.push(data));
+    console.log(this.output);
   }
 
   onClick_uiResult() {
     this.onClick_postReach();
-    console.log(this.output);
+    this.showResult = true;
+    //console.log(this.arrayOut);
   }
 
+  onClick_clear() {
+    this.mylist = null;
+    this.output = null;
+    this.showResult = false;
+  }
+
+  checkList() {
+    if (!Array.isArray(this.mylist) || !this.mylist.length) {
+      this.onClick_addReach();
+    }
+  }
+  //objectKeys(obj) {
+  //  return Object.keys(obj);
+  //}
+  
   /*drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.mylist, event.previousIndex, event.currentIndex);
   }*/ //used for object rearrangement
 }
+
+
