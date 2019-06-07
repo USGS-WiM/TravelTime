08d49d93d82d98e840649f16955af4757c064840
tim7en <sabitov.ty@gmail.com>
2019-06-06 22:36:27 -0400

    Got a better control over marker layers, finally they are reactive. Changed functions in services and map components.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -4,7 +4,7 @@ import { GetNavigationService } from '../services/get-navigation.service';
 import { MapService } from '../services/map.service';
 import {site, parameters} from '../site';
 import {myfunctions} from '../shared/myfunctions';
-
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: "app-map",
@@ -14,13 +14,9 @@ import {myfunctions} from '../shared/myfunctions';
 
 export class MapComponent extends myfunctions implements OnInit {
 
-  Site_reference: site;
-  results = [];
-  showLayer: boolean;
-
   constructor(
     private _GetNavigationService: GetNavigationService,
-    private _MapService: MapService
+    private _MapService: MapService,
   ) { super () }
 
   ngOnInit() {
@@ -29,28 +25,91 @@ export class MapComponent extends myfunctions implements OnInit {
       this.Site_reference = data['configuration'];
     }); //get service {description: Initial description}
   }
+
+  layersControl = {
+    baseLayers: {
+      'OpenStreetMap': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        zIndex: 1,
+        attribution:
+        'Imagery from <a href="https://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a>' +
+        '&mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }),
+
+      'Topo': L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+        zIndex: 1,
+        attribution:
+          'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL,' +
+          'Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+      }),
+
+      'TexasHydro': L.tileLayer(
+        "https://txgeo.usgs.gov/arcgis/rest/services/Mapping/HydroBaseMapForTerrain/MapServer/tile/{z}/{y}/{x}",
+        {
+          maxZoom: 11,
+          minZoom: 5,
+          attribution: 'Texas Streamer Esri product'
+        }
+      )
+    }
+  }
+
+  // Values to bind to Leaflet Directive
+	options = {
+		layers: [ this.layersControl.baseLayers.Topo],
+		zoom: 10,
+		center: L.latLng(39.6, -75.7)
+  };
   
-  onMapClick(e) {
-    this.showLayer = true;
+
+  userselect_markers: L.Marker [] = [];
+  sitesupstream_markers: L.Marker [] = [];
+  sitesdownstream_markers: L.Marker [] = [];
+  downstream_polyline: L.Layer [] = [];
+  layers: L.Layer [] = [];
+
+  Site_reference: site;
+  results = [];
+  sites_upstream = [];
+  sites_downstream = [];
+  fitBounds: any = null;
+
+  marker_sites = [];
+
+
+  addMarker(e) {
     let mySite = new site ([this.Site_reference]);
+    if (this.userselect_markers.length>0){
+      while (this.userselect_markers.length != 0) {
+        this.userselect_markers.splice(0, 1)
+      }
+    }
     const marker = new L.marker([e.latlng.lat, e.latlng.lng], {
-      //draggable: true,
       icon: L.icon({
         iconUrl: "https://unpkg.com/leaflet@1.4.0/dist/images/marker-icon.png",
         iconSize: [25, 35],
         iconAnchor: [30 / 2, 35]
       })
-    }).on('click',
-      this.swapElement(mySite['mylist'], 2, 3));
+    }).on('click',this.swapElement(mySite['mylist'], 2, 3));
+    this.userselect_markers.push(marker);
+    this.marker_sites.push(mySite);
+  }
 
-    this.onMarkerClick(mySite['mylist'], e.latlng.lat, e.latlng.lng, 'upstream', ['gage'], 100);
-    this.onMarkerClick(mySite['mylist'], e.latlng.lat, e.latlng.lng, 'downstream', ['gage', 'flowline'], 100);
-    this._MapService.markers.push(marker);
-    this._MapService.sites.push(marker);
+  getUpstream (){
+    let mySite = this.marker_sites[this.marker_sites.length-1];
+    let markerup = this.userselect_markers[0];
+    let e = markerup.getLatLng();
+    this.onMarkerClick(mySite['mylist'], e.lat, e.lng, 'upstream', ['gage'], 100);
+  }
+
+  getDownstream(){
+    let mySite = this.marker_sites[this.marker_sites.length-1];
+    let markerdw = this.userselect_markers[0];
+    let e = markerdw.getLatLng();
+    this.onMarkerClick(mySite['mylist'], e.lat, e.lng, 'downstream', ['gage', 'flowline'], 100);
   }
 
   onMarkerClick(e, lat, lng, cond, option, len) {
-
     e[0]['value']['coordinates'] = [ lng, lat]; // add lat long
     e[1]['value'] = cond;
     e[3]['value'] = option;
@@ -61,17 +120,18 @@ export class MapComponent extends myfunctions implements OnInit {
       let myvar = new parameters (e[2].value[0]);
       e[2].value = myvar;
     }
-
     this._GetNavigationService.postGage(e)
       .toPromise().then(data => {
+        let myreturn;
+        let polyline;
         if (cond == 'upstream') {
-          this._MapService.getUpstream(data);
-
+          myreturn = this._MapService.getUpstream(data);
         } else {
-          this._MapService.getDownstream(data);
-          this.showLayer = false;
+          myreturn = this._MapService.getDownstream(data);
+          polyline = this._MapService.addPolyLine(data);
+          this.userselect_markers.push(polyline);
         }
-        this.results.push(data)
+        this.userselect_markers.push(myreturn);
       }
     );
 
--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -5,7 +5,7 @@ import { latLng, tileLayer, Layer } from 'leaflet';
 import {POINT} from '../reach';
 import * as L from "leaflet";
 import { myfunctions } from '../shared/myfunctions'; 
-
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,17 +13,11 @@ import { myfunctions } from '../shared/myfunctions';
 
 export class MapService extends myfunctions {
   public result = [];
-  public mySite: site;
-  public mymap: L.Map;
-  public markers: Layer[] = [];
-  public sites: Layer[] = [];
+
   public myPoint;
   public layersControl;
   public streamLine: Layer[] = [];
   public streamArray = [];
-
-  public gagesUpstream: Layer[] = [];
-  public gagesDownstream: Layer[] = [];
 
   public gagesUpstreamArray = [];
   public gagesDownstreamArray = [];
@@ -31,90 +25,23 @@ export class MapService extends myfunctions {
 
   constructor() {
     super()
-
-    this.mymap = {
-      layers: [
-        tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', { maxZoom: 20, attribution: '...' })
-      ],
-      zoom: 5,
-      center: latLng(39.6, -75.7)
-    };
-
-    this.layersControl = {
-      baseLayers: {
-        'OpenStreetMap': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 20,
-          zIndex: 1,
-          attribution:
-          'Imagery from <a href="https://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a>' +
-          '&mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }),
-  
-        'Topo': L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-          zIndex: 1,
-          attribution:
-            'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL,' +
-            'Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
-        }),
-        
-        'CartoDB': L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-          zIndex: 1,
-          attribution:
-              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; ' +
-                  '<a href="https://cartodb.com/attributions">CartoDB</a>'
-        }),
-  
-        'Satellite': L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-          zIndex: 1,
-          attribution:
-              'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, ' +
-                  'and the GIS User Community'
-            // maxZoom: 10
-        }),
-  
-        'Terrain': L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
-          zIndex: 1,
-          attribution: 'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
-          maxZoom: 13
-        }),
-  
-        'Gray': L.tileLayer(
-          'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
-          {
-              zIndex: 1,
-              attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-              maxZoom: 16
-          }
-        ),
-        'TexasHydro': L.tileLayer(
-          "https://txgeo.usgs.gov/arcgis/rest/services/Mapping/HydroBaseMapForTerrain/MapServer/tile/{z}/{y}/{x}",
-          {
-            maxZoom: 11,
-            minZoom: 5,
-            attribution: 'Texas Streamer Esri product'
-          }
-        )
-        
-      },
-      overlays: {
-  
-      }
-    }
   }
 
-  getUpstream(data) {
+  //get upstream data from the marker
+  getUpstream (data) {
+    while (this.gagesUpstreamArray.length != 0) {
+      this.gagesUpstreamArray.splice(0, 1)
+    }
     for (var i = 1; i < data['features'].length; i++) { //first one is the user selected site
       var gagesUpstream = this.deepCopy(data['features'][i]);
       this.gagesUpstreamArray.push(gagesUpstream);
     }
-
     function onEachFeature(feature, layer) {
       // does this feature have a property named 'name, comid,uri, source'?
       if (feature.properties && feature.properties.name) {
-        layer.bindPopup(feature.properties.comid + " "+ feature.properties.name + " " + feature.properties.uri + " "+ feature.properties.source);
+        layer.bindPopup(feature.properties.comid + " " + feature.properties.name + " " + feature.properties.uri + " " + feature.properties.source);
       }
     };
-
     var gagesUpstream = L.geoJSON(this.gagesUpstreamArray, {
       onEachFeature: onEachFeature,
       pointToLayer: function (feature, latlng) {
@@ -131,25 +58,26 @@ export class MapService extends myfunctions {
         });
       }
     });
-
-    this.sites.push(gagesUpstream);
+    return (gagesUpstream);
   };
 
-  getDownstream(data) {
+  //get downstream data from the marker
+  getDownstream (data) {
+    while (this.gagesDownstreamArray.length != 0) {
+      this.gagesDownstreamArray.splice(0, 1)
+    }
     for (var i = 1; i < data['features'].length; i++) { //first one is the user selected site
       if (data['features'][i].geometry['type'] == 'Point') { //if type of point, add marker
         var gagesDownstream = this.deepCopy(data['features'][i]);
         this.gagesDownstreamArray.push(gagesDownstream);
       }
     }
-
     function onEachFeature(feature, layer) {
       // does this feature have a property named 'name, comid,uri, source'?
       if (feature.properties && feature.properties.name) {
         layer.bindPopup(feature.properties.comid + " " + feature.properties.name + " " + feature.properties.uri + " " + feature.properties.source);
       }
     };
-
     var gagesDownstream = L.geoJSON(this.gagesDownstreamArray, {
       onEachFeature: onEachFeature,
       pointToLayer: function (feature, latlng) {
@@ -166,22 +94,19 @@ export class MapService extends myfunctions {
         });
       }
     });
-
-    this.sites.push(gagesDownstream);
-    this.addPolyLine(data);
+    return (gagesDownstream);
   };
 
-  addPolyLine(data) {
-
+  //add polyline downstream
+  addPolyLine (data) {
     for (var i = 1; i < data['features'].length; i++) {
       if (data['features'][i].geometry['type'] == 'LineString') { //if type of point, add marker
         var polylinePoints = this.deepCopy(data['features'][i]);
         this.streamArray.push(polylinePoints);
       }
     }
-
     this.streamLine = L.geoJSON(this.streamArray);
-    this.sites.push(this.streamLine);
+    return (this.streamLine);
   }
 
 }
